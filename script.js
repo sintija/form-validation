24d4e6839bee58041bca98b808c5fe0608e6e676
sintija <sintija.linuza@gmail.com>
2021-02-07 23:21:48 +0000

    Refactoring js form input validations

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ let password = document.querySelector("#password");
 let password2 = document.querySelector("#password2");
 
 
+//check if email is valid
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
 //show the error message
 function showError(input, message) {
     //outline input with red
@@ -19,40 +26,34 @@ function showSuccess(input) {
     //outline input with red
     let formControl = input.parentElement;
     formControl.className = 'form-control sucess';
+}
+
+
+//check required fields 
+function checkRequired(inputArr) {
+    inputArr.forEach(function (input) {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} required`);
+        } else {
+            showSuccess(input);
+        }
+    });
 
 }
+
+//get field name and capitalize the first letter 
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 
 
 function submitForm(event) {
     event.preventDefault();
     // console.log(username.value);
+    checkRequired([username, email, password, password2]);
 
-    if (username.value === '') {
-        showError(username, 'Username is required');
-
-    } else {
-        showSuccess(username);
-    }
-    if (email.value === '') {
-        showError(email, 'Email is required');
-
-    } else {
-        showSuccess(email);
-    }
-
-    if (password.value === '') {
-        showError(password, 'password is required');
-
-    } else {
-        showSuccess(password);
-    }
-
-    if (password2.value === '') {
-        showError(password2, 'Password is required');
-
-    } else {
-        showSuccess(password2);
-    }
 }
 
 
