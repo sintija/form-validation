42f97e3f94ac2188d9ab07eb5f7e67fcbc66e41a
sintija <sintija.linuza@gmail.com>
2021-02-08 20:09:19 +0000

    add length to iputs and check if passwords match

--- script.js
+++ script.js
@@ -8,8 +8,27 @@ let password2 = document.querySelector("#password2");
 //check if email is valid
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+
+    if (re.test(email.value.trim())) {
+        showSuccess(email);
+    } else {
+        showError(email, 'Email is not valid');
+    }
+
 }
+
+//check if the passwords match 
+function checkPasswordMatch(input1, input2) {
+    if (input1.value === input2.value) {
+        showSuccess(input);
+    } else {
+        showError(input2, "Passwords do not match");
+    }
+}
+
+
+
+
 
 
 //show the error message
@@ -48,11 +67,27 @@ function getFieldName(input) {
 }
 
 
+//Check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length < max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
 
 function submitForm(event) {
     event.preventDefault();
     // console.log(username.value);
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    validateEmail(email);
+    checkPasswordMatch(password, password2);
+
 
 }
 
