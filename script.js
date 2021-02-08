8fd95d2f54e4adea7802c430ea4e775766aeadce
sintija <sintija.linuza@gmail.com>
2021-02-08 20:32:42 +0000

    input length message fix

--- script.js
+++ script.js
@@ -28,9 +28,6 @@ function checkPasswordMatch(input1, input2) {
 
 
 
-
-
-
 //show the error message
 function showError(input, message) {
     //outline input with red
@@ -71,7 +68,7 @@ function getFieldName(input) {
 function checkLength(input, min, max) {
     if (input.value.length < min) {
         showError(input, `${getFieldName(input)} must be at least ${min} characters`);
-    } else if (input.value.length < max) {
+    } else if (input.value.length > max) {
         showError(input, `${getFieldName(input)} must be less than ${max} characters`);
     } else {
         showSuccess(input);
